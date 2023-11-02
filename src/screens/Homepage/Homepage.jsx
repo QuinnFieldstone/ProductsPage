058c280104cdc90bebb05c6d983bf95a338f5f5e
Quinn <quinnfieldstone98@gmail.com>
2023-11-02 00:46:52 -0400

    adds all files

---
+++ src/screens/Homepage/Homepage.jsx
@@ -0,0 +1,431 @@
+import React from "react";
+import { ComercialShopping } from "../../components/ComercialShopping";
+import { InterfaceEssential } from "../../components/InterfaceEssential";
+import { LayoutGrid } from "../../components/LayoutGrid";
+import "./style.css";
+
+export const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="div-3">
+        <div className="overlap-9">
+          <div className="rectangle-7" />
+          <div className="background">
+            <div className="overlap-group-7">
+              <img className="vector" alt="Vector" src="/img/vector-4.png" />
+              <img className="union" alt="Union" src="/img/union.png" />
+              <img className="union-2" alt="Union" src="/img/union-1.png" />
+              <img className="union-3" alt="Union" src="/img/union-2.png" />
+            </div>
+          </div>
+          <div className="free-discount">
+            <div className="overlap-10">
+              <div className="rectangle-8" />
+              <div className="dicount-icon">
+                <div className="overlap-group-8">
+                  <div className="rectangle-9" />
+                  <img className="comercial-discount" alt="Comercial discount" src="/img/comercial-discount-2.png" />
+                </div>
+              </div>
+              <p className="get-off-your">
+                Get 20% Off
+                <br />
+                Your 1st Order
+              </p>
+              <p className="you-get-over">
+                <span className="text-wrapper-50">You get over </span>
+                <span className="text-wrapper-51">$99</span>
+              </p>
+              <div className="text-wrapper-52">Coupon Code</div>
+              <div className="coupon-code">
+                <div className="overlap-11">
+                  <div className="rectangle-10" />
+                  <div className="text-wrapper-53">FIRST20</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="freee-shipping-card">
+            <div className="overlap-10">
+              <div className="rectangle-11" />
+              <div className="free-express">
+                Free Express
+                <br />
+                Shipping
+              </div>
+              <p className="orders-above">
+                <span className="text-wrapper-54">Orders above </span>
+                <span className="text-wrapper-51">$150</span>
+              </p>
+              <div className="text-wrapper-55">Discreet, Undetectable</div>
+              <div className="delivery-van">
+                <div className="overlap-group-8">
+                  <div className="rectangle-12" />
+                  <ComercialShopping style="outlined" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hot-banner">
+            <div className="overlap-12">
+              <img className="rectangle-13" alt="Rectangle" />
+              <p className="text-wrapper-56">
+                “Psychedelics prove to you that there’s more than one way of seeing the world”
+              </p>
+              <button className="button">
+                <div className="overlap-group-9">
+                  <div className="rectangle-14" />
+                  <div className="text-wrapper-57">Shop Now</div>
+                </div>
+              </button>
+              <div className="text-wrapper-58">Jesse Lawler</div>
+              <div className="pignation">
+                <div className="ellipse-wrapper">
+                  <div className="ellipse-4" />
+                </div>
+                <div className="ellipse-5" />
+                <div className="ellipse-6" />
+              </div>
+            </div>
+          </div>
+          <div className="top-categories">
+            <div className="text-wrapper-59">Top Categories</div>
+            <div className="overlap-13">
+              <div className="frame-5">
+                <div className="wavyz-x-wrapper">
+                  <img className="wavyz-x" alt="Wavyz x" src="/img/wavyz3-1-300x300-1.png" />
+                </div>
+                <div className="dried-magic">
+                  Dried Magic
+                  <br />
+                  Mushrooms
+                </div>
+                <div className="hover-item">
+                  <div className="group-11">
+                    <div className="overlap-group-10">
+                      <div className="text-wrapper-60">Shop Now</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-7">
+                <div className="wavyz-x-wrapper">
+                  <img className="wavyz-x" alt="Wavyz x" src="/img/wavyz3-1-300x300-1-1.png" />
+                </div>
+                <div className="dried-magic">
+                  Shrom <br />
+                  Edibles
+                </div>
+                <div className="hover-item">
+                  <div className="group-11">
+                    <div className="overlap-group-10">
+                      <div className="text-wrapper-60">Shop Now</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-8">
+                <div className="wavyz-x-wrapper">
+                  <img className="wavyz-x" alt="Wavyz x" src="/img/wavyz3-1-300x300-1-2.png" />
+                </div>
+                <div className="dried-magic-2">
+                  Magic <br />
+                  Mushrooms Tea
+                </div>
+                <div className="hover-item">
+                  <div className="group-11">
+                    <div className="overlap-group-10">
+                      <div className="text-wrapper-60">Shop Now</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-9">
+                <div className="frame-10" />
+                <div className="dried-magic">
+                  Microdosing
+                  <br />
+                  Capsules
+                </div>
+                <div className="hover-item">
+                  <div className="group-11">
+                    <div className="overlap-group-10">
+                      <div className="text-wrapper-60">Shop Now</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="main-top-nav">
+            <div className="mushroom-2">MUSHROOM</div>
+            <div className="marijuana">MARIJUANA</div>
+            <div className="img-wrapper">
+              <img className="comercial-shopping-2" alt="Comercial shopping" src="/img/comercial-shopping-basket.png" />
+            </div>
+            <div className="frame-11">
+              <div className="search-2">
+                <div className="overlap-group-11">
+                  <InterfaceEssential style="outlined" />
+                  <div className="text-wrapper-61">What do you want</div>
+                </div>
+              </div>
+            </div>
+            <div className="wish-list-wrapper">
+              <div className="wish-list-2">
+                <img className="icon-2" alt="Icon" src="/img/icon.png" />
+              </div>
+            </div>
+            <div className="account-sign-in-wrapper">
+              <div className="account-sign-in">
+                <div className="overlap-group-12">
+                  <div className="text-wrapper-62">Sign in</div>
+                  <LayoutGrid style="outlined" />
+                </div>
+              </div>
+            </div>
+            <img className="layer-2" alt="Layer" src="/img/layer-1-2.png" />
+          </div>
+          <div className="text-wrapper-63">Canada’s</div>
+          <div className="text-wrapper-64">Best Spot to</div>
+          <p className="buy-shroms-online">
+            <span className="text-wrapper-65">Buy </span>
+            <span className="text-wrapper-66">Shroms</span>
+            <span className="text-wrapper-67">&nbsp;</span>
+            <span className="text-wrapper-65">Online!</span>
+          </p>
+        </div>
+        <div className="about-US">
+          <div className="overlap-14">
+            <img className="rectangle-15" alt="Rectangle" src="/img/rectangle-12.png" />
+            <p className="text-wrapper-68">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore.
+            </p>
+            <img className="ellipse-7" alt="Ellipse" src="/img/ellipse-16.png" />
+            <div className="about-text">
+              <div className="overlap-group-13">
+                <p className="lorem-ipsum-dolor">
+                  <span className="text-wrapper-69">Lorem ipsum dolor sit a sectetur </span>
+                  <span className="text-wrapper-70">adipiscing</span>
+                  <span className="text-wrapper-69"> elit sed do </span>
+                  <span className="text-wrapper-70">eiusmod</span>
+                  <span className="text-wrapper-69"> tempor </span>
+                </p>
+                <p className="text-wrapper-71">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore.
+                </p>
+                <div className="overlap-15">
+                  <div className="rectangle-16" />
+                  <p className="text-wrapper-72">Lorem ipsum dolor sit amet,</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overlap-16">
+            <p className="knowing-us">
+              <span className="text-wrapper-73">Knowing</span>
+              <span className="text-wrapper-70"> us</span>
+            </p>
+            <div className="text-wrapper-74">Getting to</div>
+          </div>
+        </div>
+        <div className="text-wrapper-75">How your order works</div>
+        <div className="text-wrapper-76">let’s get your started</div>
+        <div className="highlights">
+          <div className="overlap-17">
+            <div className="overlap-18">
+              <div className="text-wrapper-77">Some Awesome</div>
+              <div className="text-wrapper-78">Review Highlights</div>
+            </div>
+            <div className="pignation-2">
+              <div className="ellipse-wrapper">
+                <div className="ellipse-4" />
+              </div>
+              <div className="ellipse-5" />
+              <div className="ellipse-6" />
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-19">
+                <div className="review-profile">
+                  <div className="group-12">
+                    <img className="ellipse-8" alt="Ellipse" src="/img/ellipse-18.png" />
+                  </div>
+                </div>
+                <p className="text-wrapper-79">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua
+                </p>
+                <div className="text-wrapper-80">John Doe</div>
+              </div>
+            </div>
+            <div className="group-13">
+              <div className="overlap-19">
+                <div className="group-wrapper">
+                  <div className="group-12">
+                    <img className="ellipse-8" alt="Ellipse" src="/img/ellipse-18-1.png" />
+                  </div>
+                </div>
+                <p className="text-wrapper-81">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua
+                </p>
+                <div className="text-wrapper-82">Mary Jane</div>
+              </div>
+            </div>
+            <div className="arrows-chevron-wrapper">
+              <img className="arrows-chevron" alt="Arrows chevron" src="/img/arrows-chevron-chevron-right-1.png" />
+            </div>
+            <div className="cart-2">
+              <img className="arrows-chevron-2" alt="Arrows chevron" src="/img/arrows-chevron-chevron-right-2.png" />
+            </div>
+          </div>
+        </div>
+        <div className="perks">
+          <p className="top-perks-for-you">
+            <span className="text-wrapper-73">Top</span>
+            <span className="text-wrapper-70"> Perks</span>
+            <span className="text-wrapper-73"> for you</span>
+          </p>
+          <div className="frame-12">
+            <div className="element-wrapper">
+              <img className="element-2" alt="Element" src="/img/0-1.png" />
+            </div>
+            <div className="frame-13">
+              <img className="element-2" alt="Element" src="/img/0-1-7.png" />
+            </div>
+            <div className="frame-14">
+              <img className="element-2" alt="Element" src="/img/0-1-8.png" />
+            </div>
+            <div className="frame-15">
+              <img className="element-2" alt="Element" src="/img/0-1-9.png" />
+            </div>
+          </div>
+          <div className="frame-16">
+            <div className="group-14">
+              <div className="overlap-20">
+                <div className="frame-17">
+                  <div className="overlap-group-14">
+                    <div className="rectangle-17" />
+                    <img className="dames-grape" alt="Dames grape" src="/img/0-1-9.png" />
+                  </div>
+                </div>
+                <p className="lorem-ipsum-dolor-2">
+                  <span className="text-wrapper-69">Lorem </span>
+                  <span className="text-wrapper-70">ipsum dolor</span>
+                  <span className="text-wrapper-69"> sit amet consectetur </span>
+                </p>
+                <p className="text-wrapper-83">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore.
+                </p>
+                <div className="top-product-sale-2">
+                  <div className="text-wrapper-84">ipsum dolos</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-21">
+          <div className="subscribtion">
+            <div className="overlap-22">
+              <div className="overlap-group-15">
+                <div className="text-wrapper-85">Your email address</div>
+                <div className="text-wrapper-86">Subscribe</div>
+              </div>
+              <div className="text-wrapper-87">Subscribing to our newsletters</div>
+              <p className="text-wrapper-88">Get weekly insights on new products and offers avialable</p>
+            </div>
+          </div>
+          <div className="top-product-sale-3">
+            <div className="text-wrapper-89">Subscribe</div>
+          </div>
+        </div>
+        <div className="group-15">
+          <div className="overlap-23">
+            <div className="overlap-group-16">
+              <img className="group-16" alt="Group" src="/img/group-6-1.png" />
+              <p className="we-serve-all-2">
+                We serve all&nbsp;&nbsp;
+                <br />
+                Provinces <br />
+                in Canada
+              </p>
+            </div>
+            <p className="we-are-delighted-to-2">We are delighted&nbsp;&nbsp;to let you know</p>
+          </div>
+        </div>
+        <div className="order-process-2">
+          <div className="overlap-24">
+            <div className="overlap-25">
+              <div className="rectangle-18" />
+              <div className="ellipse-9" />
+              <div className="text-wrapper-90">1</div>
+              <div className="text-wrapper-91">Place your Order</div>
+            </div>
+            <div className="overlap-26">
+              <div className="rectangle-19" />
+              <div className="ellipse-10" />
+              <div className="text-wrapper-92">2</div>
+              <div className="text-wrapper-93">2</div>
+              <div className="text-wrapper-94">Receive a Confirmation Email</div>
+            </div>
+            <div className="overlap-group-17">
+              <div className="rectangle-19" />
+              <div className="ellipse-10" />
+              <div className="text-wrapper-95">3</div>
+              <div className="text-wrapper-96">Receive Product Tracking Details</div>
+            </div>
+            <div className="overlap-27">
+              <div className="rectangle-20" />
+              <div className="ellipse-9" />
+              <div className="text-wrapper-97">4</div>
+              <p className="text-wrapper-98">Get Product to your Door Step</p>
+            </div>
+            <p className="we-aim-to-provide-2">We Aim To Provide The Best Customer Experience</p>
+          </div>
+        </div>
+        <div className="footer-wrapper">
+          <div className="footer-2">
+            <div className="overlap-group-18">
+              <div className="text-wrapper-99">Follow Us</div>
+              <div className="copyright-three-2">Copyright @ 2023&nbsp;&nbsp;Three Amigos</div>
+              <div className="terms-conditions-2">Terms &amp; Conditions</div>
+              <div className="text-wrapper-100">Privacy Policy</div>
+              <div className="text-wrapper-101">Sales and Refuds</div>
+              <div className="text-wrapper-102">Interested Links</div>
+              <div className="mushroom-species">Mushroom Spices</div>
+              <div className="text-wrapper-103">Cannabises Strains</div>
+              <div className="text-wrapper-104">FAQ</div>
+              <div className="text-wrapper-105">Blog</div>
+              <div className="mirodosing-capsules">Saprotrophic</div>
+              <div className="text-wrapper-106">Hybrid</div>
+              <div className="text-wrapper-107">Dried Magic Mashrooms</div>
+              <div className="text-wrapper-108">Sativa</div>
+              <div className="text-wrapper-109">Edibles</div>
+              <div className="text-wrapper-110">Indica</div>
+              <div className="text-wrapper-111">Teas</div>
+              <div className="text-wrapper-112">Coupons</div>
+              <div className="text-wrapper-113">Contact</div>
+              <img className="line-3" alt="Line" src="/img/line-1.png" />
+              <img className="positivessl-2" alt="Positivessl" />
+              <div className="cart-3">
+                <img
+                  className="interface-essential-2"
+                  alt="Interface essential"
+                  src="/img/interface-essential-facebook.png"
+                />
+              </div>
+              <div className="cart-4">
+                <img className="icon-3" alt="Icon" src="/img/icon-1.png" />
+              </div>
+              <div className="rectangle-21" />
+              <div className="rectangle-22" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
